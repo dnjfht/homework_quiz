a720a6b7e34a552333cb10fa6df2878dbdecb98b
Yoo Seungmin <dnjfht@gmail.com>
2023-04-01 08:22:08 +0900

    퀴즈 화면 레이아웃 수정

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,5 +1,10 @@
 import React from "react";
 import styled from "styled-components";
+import { useSelector, useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import img01 from "../img/img01.png";
+import { addAnswer } from "../redux/modules/quiz";
 
 const QuizWrap = styled.div`
   width: 320px;
@@ -21,12 +26,110 @@ const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  justify-content: space-between;
+`;
+
+const TitleWrap = styled.div`
+  width: 100%;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const AnswerTitle = styled.p`
+  width: 120px;
+  height: 40px;
+  padding: 10px;
+  box-sizing: border-box;
+  background-color: rgb(213, 210, 247);
+  border-radius: 50px;
+
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Question = styled.h3`
+  margin: 0;
+
+  font-size: 21px;
+`;
+
+const ImageWrap = styled.img`
+  width: 100%;
+`;
+
+const WrapSelectAnswer = styled.div`
+  width: 100%;
+
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+const SelectAnswer = styled.button`
+  background-color: transparent;
+  border: none;
+
+  font-size: 80px;
+  font-weight: 600;
+  color: rgb(213, 210, 247);
 `;
 
 export default function Quiz() {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const quiz_list = useSelector((state) => state.quiz.quiz_list);
+  console.log(quiz_list);
+  const user_answer_list = useSelector((state) => state.quiz.user_answer_list);
+  console.log(user_answer_list);
+
+  const setAnswer = (user_answer) => {
+    dispatch(addAnswer(user_answer));
+  };
+
+  useEffect(() => {
+    if (user_answer_list.length === quiz_list.length) {
+      navigate("/score");
+      return;
+    }
+  }, [user_answer_list]);
+
+  if (user_answer_list.length === quiz_list.length) {
+    return null;
+  }
+
   return (
     <QuizWrap>
-      <Container>Quiz</Container>
+      <Container>
+        <TitleWrap style={{ textAlign: "center" }}>
+          <AnswerTitle>{user_answer_list.length + 1}번 문제</AnswerTitle>
+
+          <Question>{quiz_list[user_answer_list.length].question}</Question>
+        </TitleWrap>
+
+        <ImageWrap src={img01} />
+
+        <WrapSelectAnswer>
+          <SelectAnswer
+            onClick={() => {
+              setAnswer(true);
+            }}
+          >
+            O
+          </SelectAnswer>
+          <SelectAnswer
+            onClick={() => {
+              setAnswer(false);
+            }}
+          >
+            X
+          </SelectAnswer>
+        </WrapSelectAnswer>
+      </Container>
     </QuizWrap>
   );
 }
--- src/components/Score.js
+++ src/components/Score.js
@@ -1,5 +1,67 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
+
+const ScoreWrap = styled.div`
+  width: 320px;
+  height: 550px;
+  padding: 10px;
+  box-sizing: border-box;
+  background-color: #fff;
+  margin: 0 auto;
+  border: 1px solid #ccc;
+`;
+
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  padding: 30px;
+  box-sizing: border-box;
+  background-color: #f4f4f4;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
 
 export default function Score() {
-  return <div>Score</div>;
+  const navigate = useNavigate();
+
+  const quiz_list = useSelector((state) => state.quiz.quiz_list);
+  const user_answer_list = useSelector((state) => state.quiz.user_answer_list);
+  const nickName = useSelector((state) => state.user.nickName);
+  console.log(nickName);
+
+  const _score =
+    (100 / quiz_list.length) *
+    quiz_list.filter((q, idx) => {
+      return q.answer === user_answer_list[idx];
+    }).length;
+
+  const score = Math.round(_score);
+  console.log(score);
+
+  return (
+    <ScoreWrap>
+      <Container>
+        <h3>
+          {nickName} 퀴즈에 대한 내 점수는 <br />
+          {score}점
+        </h3>
+
+        <p>우와! 우린 참 친해요!</p>
+
+        <button>{nickName}에게 한 마디</button>
+
+        <button
+          onClick={() => {
+            navigate("/");
+          }}
+        >
+          시작 화면으로 이동
+        </button>
+      </Container>
+    </ScoreWrap>
+  );
 }
